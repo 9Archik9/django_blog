fa5d950ac03eaef8151c2fa728a990007d5b7e3b
Artyom <61574740+9Archik9@users.noreply.github.com>
2024-03-10 18:52:28 +0300

    first part. without hw

---
+++ django_blog/blog/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+from blog import views
+
+urlpatterns = [
+
+    path('', views.post_iist, name='post_list'),
+    path('post/<int:post_id>/', views.post_detail, name='post'),
+    path('create_post/', views.create_post, name='create_post'),
+    path('tags/<tag_slug>/', views.posts_by_tag, name='posts_by_tag')
+
+]
